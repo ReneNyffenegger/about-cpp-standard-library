44da818295c6564b2f8408009f803838346b9b1b
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-11 07:37:34 +0200

    + array/iterate.cpp

---
+++ array/iterate.cpp
@@ -0,0 +1,15 @@
+// 
+// with g++ -std=c++11 iterate.cpp
+//
+#include <array>
+#include <iostream>
+
+int main() {
+
+  std::array<int, 6> a = {1, 1, 2, 3, 5, 8};
+
+  for (int i : a) {
+    std::cout << i << std::endl;
+  }
+
+}
