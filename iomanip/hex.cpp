d3a22a8fc9fe254c1e94f56d2da91f9de8dba69f
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-02-09 19:42:16 +0100

    + hex.cpp

---
+++ iomanip/hex.cpp
@@ -0,0 +1,15 @@
+//
+// g++ hex.cpp
+//
+#include <iostream>
+#include <iomanip>
+
+
+int main() {
+
+    std::cout             << 100   << std::endl;
+    std::cout << std::hex << 100   << std::endl;
+    std::cout <<             100.1 << std::endl;
+    std::cout << std::dec << 100   << std::endl;
+
+}
